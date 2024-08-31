cd956dd5a2898710b9d1c733e37310f552ede716
J Pedro Rivero P <129573462+PedroRiverop@users.noreply.github.com>
2024-08-31 03:03:23 +0000

    Correccion de espaciador de nominio ||

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ window.onload = function() {
   for (let i = 0; i < pronoun.length; i++) {
     for (let j = 0; j < adj.length; j++) {
       for (let k = 0; k < noun.length; k++) {
-        combinacion.push([pronoun[i] + adj[j] + noun[k] + ".com //  "]);
+        combinacion.push([pronoun[i] + adj[j] + noun[k] + ".com ||  "]);
       }
     }
   }
